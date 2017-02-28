66aa8727d4872d2456f246bac663bc02569503eb
Hid <mikeygulati@gmail.com>
2017-02-27 20:25:55 -0500

    started work on driver class

--- src/main/java/hw3/SlideShowManager.java
+++ src/main/java/hw3/SlideShowManager.java
@@ -1,4 +1,28 @@
 package hw3;
 
+import java.util.Scanner;
+
 public class SlideShowManager {
+    private final Scanner scanner;
+    private final UndoRedoStack slideshow;
+
+    public SlideShowManager() {
+        this.scanner = new Scanner(System.in);
+        this.slideshow = new UndoRedoStack();
+    }
+
+    private void init() {
+
+    }
+
+    private void reselect() {
+
+    }
+
+    public void openMenu() {
+        switch(scanner.nextLine().isEmpty() ? scanner.nextLine().trim().charAt(0) : '0') {
+            
+        }
+    }
+
 }
