4ae934e2369f7920db84f47bc106c365a911c468
Hid <mikeygulati@gmail.com>
2017-03-03 14:11:39 -0500

    added more skeleton code in SlideShowManager

--- src/main/java/hw3/SlideShowManager.java
+++ src/main/java/hw3/SlideShowManager.java
@@ -1,27 +1,61 @@
 package hw3;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class SlideShowManager {
     private final Scanner scanner;
-    private final UndoRedoStack slideshow;
+    private final UndoRedoStack commands;
+    private final List<String> slideshow;
 
     public SlideShowManager() {
         this.scanner = new Scanner(System.in);
-        this.slideshow = new UndoRedoStack();
+        this.commands = new UndoRedoStack();
+        this.slideshow = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        new SlideShowManager().init();
     }
 
     private void init() {
-
+        // Print Description
+        // Print Menu
+        reselect();
     }
 
     private void reselect() {
-
+        // Select an option:
+        openMenu();
     }
 
     public void openMenu() {
         switch(scanner.nextLine().isEmpty() ? scanner.nextLine().trim().charAt(0) : '0') {
+            case 'A':
 
+                break;
+            case 'R':
+
+                break;
+            case 'S':
+
+                break;
+            case 'M':
+
+                break;
+            case 'P':
+
+                break;
+            case 'Z':
+
+                break;
+            case 'Y':
+
+                break;
+            case 'Q':
+
+                break;
         }
     }
 
