5b01cdc6ee6d37c20ecae4befa04ebed6c659479
Hid <mikeygulati@gmail.com>
2017-04-11 12:17:22 -0400

    hw6 - forgot to handle exceptions in UserDatabase

--- src/main/java/hw6/UserDatabase.java
+++ src/main/java/hw6/UserDatabase.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public class UserDatabase extends HashMap<String, User> {
 
     public void addUser(final String email, final User user) throws IllegalArgumentException {
+        if(email == null || get(email).getName().equals(user.getName())) {
+            throw new IllegalArgumentException();
+        }
+
         put(email, user);
     }
 
@@ -13,6 +17,10 @@ public class UserDatabase extends HashMap<String, User> {
     }
 
     public void removeUser(final String email) throws IllegalArgumentException {
+        if(email == null || get(email) == null) {
+            throw new IllegalArgumentException();
+        }
+
         remove(email);
     }
 }
