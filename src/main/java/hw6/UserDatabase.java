3479fb9b3b0bbbce0677896242a375afb0b3f948
Hid <mikeygulati@gmail.com>
2017-04-11 12:10:24 -0400

    hw6 - finished UserDatabase class

--- src/main/java/hw6/UserDatabase.java
+++ src/main/java/hw6/UserDatabase.java
@@ -1,4 +1,18 @@
 package hw6;
 
-public class UserDatabase {
+import java.util.HashMap;
+
+public class UserDatabase extends HashMap<String, User> {
+
+    public void addUser(final String email, final User user) throws IllegalArgumentException {
+        put(email, user);
+    }
+
+    public User getUser(final String email) {
+        return get(email);
+    }
+
+    public void removeUser(final String email) throws IllegalArgumentException {
+        remove(email);
+    }
 }
