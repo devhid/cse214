91083d89b3c61ab9c66cf04c1aa8b5e92ada21b1
Hid <mikeygulati@gmail.com>
2017-04-11 12:17:41 -0400

    hw6 - finished User class

--- src/main/java/hw6/User.java
+++ src/main/java/hw6/User.java
@@ -1,4 +1,17 @@
 package hw6;
 
 public class User {
+    private String name;
+
+    public User(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
 }
