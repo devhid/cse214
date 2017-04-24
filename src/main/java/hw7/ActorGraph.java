bb25470a92b26561fdabef6c81ca30387fcb1f03
Hid <mikeygulati@gmail.com>
2017-04-24 14:10:45 -0400

    hw7 - created skeleton for ActorGraph class

--- src/main/java/hw7/ActorGraph.java
+++ src/main/java/hw7/ActorGraph.java
@@ -1,4 +1,13 @@
 package hw7;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class ActorGraph {
+    private static HashMap<String, Actor> actorsByName;
+    private static HashMap<String, Movie> moviesByName;
+
+    public static LinkedList<String> bfs(final String name) {
+        return null;
+    }
 }
