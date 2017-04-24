9a400c54cf0bb20152ec387d46a55314e2b6715a
Hid <mikeygulati@gmail.com>
2017-04-24 14:06:09 -0400

    hw7 - created skeleton for Movie class

--- src/main/java/hw7/Movie.java
+++ src/main/java/hw7/Movie.java
@@ -1,4 +1,34 @@
 package hw7;
 
+import java.util.Set;
+
 public class Movie {
+    private String title;
+    private int year;
+
+    private Set<Actor> actors;
+
+    public Movie(final String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public Set<Actor> getActors() {
+        return this.actors;
+    }
+
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
