99308ea8ab13500fd9f34c10ed9baf5f7feb0c14
Hid <mikeygulati@gmail.com>
2017-04-25 19:30:29 -0400

    hw7 - completed Comparator classes (not-tested)

--- src/main/java/hw7/NameComparator.java
+++ src/main/java/hw7/NameComparator.java
@@ -1,4 +1,11 @@
 package hw7;
 
-public class NameComparator {
+import java.util.Comparator;
+
+public class NameComparator implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return ((Actor) o1).getName().compareTo(((Actor) o2).getName());
+    }
 }
--- src/main/java/hw7/TitleComparator.java
+++ src/main/java/hw7/TitleComparator.java
@@ -1,4 +1,10 @@
 package hw7;
 
-public class TitleComparator {
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator {
+    @Override
+    public int compare(Object o1, Object o2) {
+        return ((Movie) o1).getTitle().compareTo(((Movie) o2).getTitle());
+    }
 }
