6a86b10dc823d3d4149621acda59b6bd3c2e455d
Hid <mikeygulati@gmail.com>
2017-04-25 09:34:14 -0400

    hw7 - setup skeleton for I/O and menu operations

--- src/main/java/hw7/KBCalculator.java
+++ src/main/java/hw7/KBCalculator.java
@@ -1,7 +1,40 @@
 package hw7;
 
+import java.util.Scanner;
+
 public class KBCalculator {
+    private static final Scanner input = new Scanner(System.in);
+    private static final ActorGraph graph = new ActorGraph();
+
     public static void main(String[] args) {
-        Movie movie = new Movie("cars");
+        new KBCalculator().openMenu(true);
+    }
+
+    private void openMenu(boolean start) {
+        if(start) { System.out.print("Menu:"); }
+
+        String line = input.nextLine();
+        selectOption(line.isEmpty() ? '0' : line.trim().toUpperCase().charAt(0));
+    }
+
+    private void selectOption(final char option) {
+        switch(option) {
+            case 'I':
+                break;
+            case 'A':
+                break;
+            case 'M':
+                break;
+            case 'P':
+                break;
+            case 'B':
+                break;
+            case 'L':
+                break;
+            case 'Q':
+                break;
+            case '0':
+                break;
+        }
     }
 }
